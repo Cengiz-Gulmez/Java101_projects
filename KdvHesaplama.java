58ec5b52f711fb9e51601c2b03d123e251e61a21
cengiz-gülmez <cengiz.gulmez.92@gmail.com>
2022-09-18 15:27:51 +0300

    kdv hesaplayan program

---
+++ KdvHesaplama.java
@@ -0,0 +1,30 @@
+package Ilkpaket;
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class KdvHesaplama {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+        System.out.println("Kdv si hesaplanacak turarı giriniz:");
+        float tutar = input.nextFloat();
+        boolean karsılastırma = tutar >= 1000;
+        float kdvoranı= karsılastırma ? 0.08f:0.18f;
+        float kdvTutarı=tutar*kdvoranı;
+        float toplamTutar= tutar+kdvTutarı;
+
+        System.out.println("tutar:"+tutar);
+        System.out.println("kdv oranı:"+kdvoranı);
+        System.out.println("kdv turarı:"+kdvTutarı);
+        System.out.println("toplam tutar:"+toplamTutar);
+
+
+
+
+
+
+
+
+
+
+    }
+}
